6cbab0d6bf5d05729c6deba9bf6c03ae28e067ba
Hidden <zzdhidden@gmail.com>
2010-12-01 11:21:22 +0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monit::Application.config.secret_token = '80f06ad1adf084ac4a9ce4f3839c8e85db895483ee2f50b19e691955325228aea0ed76829d12004749654eb69551000f7544e59b6459189f40f959d9008cd128'
---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,6 @@
+#WillPaginate::ViewHelpers.pagination_options[:previous_label] = "上一页"
+#WillPaginate::ViewHelpers.pagination_options[:next_label] = "下一页"
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '<em class="icon icon-prev">上一页</em>'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '<em class="icon icon-next">下一页</em>'
+WillPaginate::ViewHelpers.pagination_options[:inner_window] = 2
+#WillPaginate::ViewHelpers.pagination_options[:class] = "ui-pagination"
